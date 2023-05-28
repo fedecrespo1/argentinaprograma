31e6b1513f3e76361ca9c8ef7636e0d95e2486e0
fedecrespo1 <fede.crespo.14@gmail.com>
2023-05-27 23:58:23 -0300

    Agrego el archivo conversionGrados.js

---
+++ P0/conversionGrados.js
@@ -0,0 +1,7 @@
+let fahrenheit = 50;
+let celsius = ( fahrenheit - 32 ) * 5/9 ;
+console.log('a. ', celsius);
+
+ fahrenheit = 75;
+  celsius = ( fahrenheit - 32 ) * 5/9 ;
+console.log('b. ', celsius);
