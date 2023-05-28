6e9e3df833a1ce0cfb31c5e12db314dafd3afe97
fedecrespo1 <134449793+fedecrespo1@users.noreply.github.com>
2023-05-28 00:02:18 -0300

    Delete conversionGrados.js

--- P0/conversionGrados.js
+++
@@ -1,7 +0,0 @@
-let fahrenheit = 50;
-let celsius = ( fahrenheit - 32 ) * 5/9 ;
-console.log('a. ', celsius);
-
- fahrenheit = 75;
-  celsius = ( fahrenheit - 32 ) * 5/9 ;
-console.log('b. ', celsius);
